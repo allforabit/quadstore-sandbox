be0f4486be80348b684e56228e5d029b0c4a8b2a
allforabit <kevnolan@gmail.com>
2021-03-08 15:53:55 +0000

    March 8, 2021 3:53 PM

--- src/query-list.ts
+++ src/query-list.ts
@@ -47,18 +47,6 @@ const queries = {
     LIMIT 100
     OFFSET 0
   `,
-  unorderedNoDate: `
-    SELECT ?id ?text ?name ?author_id ?author_name
-    WHERE {
-      ?id <ex://type> <ex://type/Item>;
-          <ex://text> ?text;
-          <ex://name> ?name.
-      ?id <ex://author> ?author_id.
-      ?author_id <ex://name> ?author_name.
-    }
-    LIMIT 100
-    OFFSET 0
-  `,
   noAuthor: `
     SELECT ?id ?text ?name
     WHERE {
@@ -87,10 +75,10 @@ const queries = {
     OFFSET 0
   `,
   count: `
-  SELECT (COUNT(?s) AS ?s)
-  WHERE {
-    ?s ?p ?o
-  }
+    SELECT (COUNT(?s) AS ?s)
+    WHERE {
+      ?s ?p ?o
+    }
   `,
 };
 
@@ -104,28 +92,25 @@ const createQueryRunner = (query: string) => async () => {
 
 const run = async () => {
   try {
-    // const withOptional = await time(createQueryRunner(queries.withOptional));
-    // const withoutOptional = await time(
-    //   createQueryRunner(queries.withoutOptional)
-    // );
-    // const unorderedNoDate = await time(
-    //   createQueryRunner(queries.unorderedNoDate)
-    // );
-    // const noAuthor = await time(createQueryRunner(queries.noAuthor));
-    // const dateTimestamp = await time(createQueryRunner(queries.dateTimestamp));
+    const withOptional = await time(createQueryRunner(queries.withOptional));
+    const withoutOptional = await time(
+      createQueryRunner(queries.withoutOptional)
+    );
+    const noAuthor = await time(createQueryRunner(queries.noAuthor));
+    const dateTimestamp = await time(createQueryRunner(queries.dateTimestamp));
     const count = await time(createQueryRunner(queries.count));
     // @ts-ignore
     console.log(`Count: ${count.value[0]["?s"].value}`);
     // console.log(dateTimestamp.time);
 
-    // console.log(
-    //   table([
-    //     ["With optional", withOptional.time],
-    //     ["Without optional", withoutOptional.time],
-    //     ["Unordered and no date", unorderedNoDate.time],
-    //     ["Simple (no linked author)", noAuthor.time],
-    //   ])
-    // );
+    console.log(
+      table([
+        ["With optional", withOptional.time],
+        ["Without optional", withoutOptional.time],
+        ["Date timestamp", dateTimestamp.time],
+        ["Simple (no linked author)", noAuthor.time],
+      ])
+    );
   } catch (e) {
     console.log(e);
   }
